daffbdac948a725a04b3ea8bf2da4ce596d576e0
Goran Jankovic <goran.jankovic@vivify.com>
2018-11-15 11:54:43 +0100

    finished typescript quickstart

--- greeter.js
+++ greeter.js
@@ -1,9 +1,23 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
+function studentGreeter(student) {
+    return 'Hello, ' + student.fullName;
+}
 var user = { firstName: 'Jane', lastName: 'User' };
 // var invalidUser = ['fsdf', 'sffsdfs'];
-document.body.innerHTML = greeter(user);
+var student = new Student('Jane', "M", "Doe");
+//document.body.innerHTML = greeter(user);
 // document.body.innerHTML = greeter(invalidUser);
 // document.body.innerHTML = greeter([]);
 // document.body.innerHTML = greeter();
+document.body.innerHTML = studentGreeter(student);
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+    fullName: string;
+    constructor (public firstName, private middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,10 +14,16 @@ function greeter(person: Person) {
     return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
 
+function studentGreeter(student: Student) {
+    return 'Hello, ' + student.fullName;
+}
+
 var user = {firstName: 'Jane', lastName: 'User'};
 // var invalidUser = ['fsdf', 'sffsdfs'];
+var student = new Student('Jane', "M", "Doe");
 
-document.body.innerHTML = greeter(user);
+//document.body.innerHTML = greeter(user);
 // document.body.innerHTML = greeter(invalidUser);
 // document.body.innerHTML = greeter([]);
 // document.body.innerHTML = greeter();
+document.body.innerHTML = studentGreeter(student);
